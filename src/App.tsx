36b83b5f0a6b0eed872d2438cba843e249972631
Ignacio Martinez <dark_nacho_xd@hotmail.cl>
2024-04-02 13:20:20 -0300

    Fixed Form Problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,166 @@
-import { FhirResource, Patient } from "fhir/r4";
 import PersonUtil from "./Services/Utils/PersonUtils";
-import ListResourceComponent from "./Components/ListResourceComponent";
-import FhirResourceService from "./Services/FhirService";
+import { DevTool } from "@hookform/devtools";
+
+import TestAutoCompleteComponent from "./Components/AutoCompleteComponets/TestAutoCompleteComponent";
 
 //Datos default de practicante
 localStorage.setItem("userRol", "Admin");
 localStorage.setItem("id", "204");
 
-const fhirService = new FhirResourceService("Patient");
+import { useForm, Controller } from "react-hook-form";
+import dayjs, { Dayjs } from "dayjs";
+import {
+  DatePicker,
+  LocalizationProvider,
+  TimePicker,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+interface FormValues {
+  practitioner: {
+    id: string;
+    display: string;
+  };
+  patient: {
+    id: string;
+    display: string;
+  };
+  day: Dayjs;
+  start: Dayjs;
+  end: Dayjs;
+}
+
 function App() {
-  function getDisplay(resource: FhirResource): string {
-    return `ID: ${resource.id}\nName: ${PersonUtil.parsePersonName(
-      resource
-    )}\nGender: ${(resource as Patient).gender || "N/A"}`;
-  }
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+    alert(JSON.stringify(data, null, 2));
+  };
+
   return (
     <>
-      root
-      <ListResourceComponent
-        searchParam={{ _count: 3 }}
-        getDisplay={getDisplay}
-        fhirService={fhirService}
-      ></ListResourceComponent>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="practitioner"
+          control={control}
+          rules={{
+            required: "selection is required",
+          }}
+          render={({ field }) => (
+            <TestAutoCompleteComponent
+              resourceType={"Practitioner"}
+              label={"Una label"}
+              getDisplay={PersonUtil.parsePersonName}
+              searchParam={"name"}
+              defaultResourceId="204"
+              onChange={(selectedObject) => {
+                if (selectedObject) {
+                  alert(JSON.stringify(selectedObject, null, 2));
+                  field.onChange({
+                    id: selectedObject.id,
+                    display: PersonUtil.parsePersonName(selectedObject),
+                  });
+                } else {
+                  field.onChange(null);
+                }
+              }}
+              textFieldProps={{
+                error: Boolean(errors.practitioner),
+                helperText: errors.practitioner && errors.practitioner.message,
+              }}
+            />
+          )}
+        />
+        <Controller
+          name="patient"
+          control={control}
+          rules={{
+            required: "selection is required",
+          }}
+          render={({ field }) => (
+            <TestAutoCompleteComponent
+              resourceType={"Patient"}
+              label={"Una label"}
+              getDisplay={PersonUtil.parsePersonName}
+              searchParam={"name"}
+              defaultResourceId="4"
+              defaultParams={{ "general-practitioner": "204" }}
+              onChange={(selectedObject) => {
+                if (selectedObject) {
+                  alert(JSON.stringify(selectedObject, null, 2));
+                  field.onChange({
+                    id: selectedObject.id,
+                    display: PersonUtil.parsePersonName(selectedObject),
+                  });
+                } else {
+                  field.onChange(null);
+                }
+              }}
+              textFieldProps={{
+                error: Boolean(errors.practitioner),
+                helperText: errors.practitioner && errors.practitioner.message,
+              }}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="day"
+          defaultValue={dayjs()}
+          render={({ field }) => (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Fecha inicio test"
+                onChange={field.onChange}
+                value={field.value}
+                inputRef={field.ref}
+                sx={{ width: "100%" }}
+              ></DatePicker>
+            </LocalizationProvider>
+          )}
+        ></Controller>
+
+        <Controller
+          control={control}
+          name="start"
+          defaultValue={dayjs()}
+          render={({ field: { onChange, value, ref } }) => (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                label="start"
+                onChange={onChange}
+                value={value}
+                inputRef={ref}
+                sx={{ width: "100%" }}
+              ></TimePicker>
+            </LocalizationProvider>
+          )}
+        ></Controller>
+
+        <Controller
+          control={control}
+          defaultValue={dayjs().add(30, "minutes")}
+          name="end"
+          render={({ field: { onChange, value, ref } }) => (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                label="end"
+                onChange={onChange}
+                value={value}
+                inputRef={ref}
+                sx={{ width: "100%" }}
+              ></TimePicker>
+            </LocalizationProvider>
+          )}
+        ></Controller>
+        <button type="submit">test</button>
+      </form>
+      <DevTool control={control} />
     </>
   );
 }
